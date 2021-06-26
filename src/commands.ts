bcdb77519dedcd2a9de4930ecbb668967f37e8ee
antonioganea <antonio.ganea@yahoo.com>
2021-06-26 17:21:44 +0300

    Scrap command code

--- src/commands.ts
+++ src/commands.ts
@@ -4,3 +4,34 @@
 // update building
 // attack
 // trade
+
+enum CommandType{
+    buildLumberjack,
+    buildMine,
+    buildWall,
+    trainTroops,
+    attackVillage
+}
+  
+namespace Command{
+    export function Add(time : number, commandType : CommandType, optionalParameters : any) : void {
+        // logica
+    }
+
+    export function GetCommandsForVillage( villageID: number ) {
+
+    }
+}
+/*
+app.get('/buildLumberjack', (req,res) => {
+    let villageID = parseInt(<any>req.query.villageID)
+    let village = World.getVillage(villageID)
+
+    let time = 1000; //in seconds
+    Command.Add(time, CommandType.buildLumberjack, {})
+
+    let village = World.getVillage(villageID)
+
+    res.send(`Building lumberjack in ${village.name}! Command will be ready in ${time} seconds.`)
+})
+*/
