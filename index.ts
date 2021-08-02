9656dc4a401cc600358aaf005037c891051b2a5c
antonioganea <antonio.ganea@yahoo.com>
2021-08-02 17:25:53 +0300

    Moved scripts into folders

--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 
-import {findUser, insertUser} from './src/database';
+import {findUser, insertUser} from './src/model/database';
 
-import { WorldPosition } from './src/utils';
-import { newMilitaryCollection, MilitaryCollection, MilitaryUnitData, MilitaryUnitType } from './src/militaryunit';
-import { User } from './src/user';
-import { Globals } from './src/queries';
+import { WorldPosition } from './src/model/utils';
+import { newMilitaryCollection, MilitaryCollection, MilitaryUnitData, MilitaryUnitType } from './src/model/militaryunit';
+import { User } from './src/model/user';
+import { Globals } from './src/model/queries';
+import { setAppPassportMiddleware } from './src/api/login';
 
 /*
 let user: User = new User("antonio");
@@ -37,79 +38,10 @@ console.log(Globals.getMapChunk(499,499,5,5))
 const app = express();
 const PORT = 80;
 
-
-
-// PASSPORT JS PART
-var passport = require('passport');
-var Strategy = require('passport-local').Strategy;
-var session = require('express-session');
-var cookieSession = require('cookie-session');
-var cookieParser = require('cookie-parser');
-
-passport.serializeUser(function(user :any, done :any) {
-  //console.log("serialization")
-  //console.log(JSON.stringify(user))
-  done(null, JSON.stringify(user));
-});
-
-passport.deserializeUser(function(user :any, done :any) {
-  //console.log("Deserialization")
-  //console.log(user)
-  done(null, JSON.parse(user)); // TODO : handle deserialization if parsing fails ( weird cookie input )
-});
-
-app.use(cookieParser('secret'));
-app.use(cookieSession({signed:false}));
-app.use(session({ secret: 'anything' }));
-
-app.use(require('body-parser').urlencoded({ extended: true }));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
-passport.use(new Strategy(
-  function(username : any, password : any, cb : any) {
-    findUser(username, function(err : any, user : any) {
-      if (err) { return cb(err); }
-      if (!user) { return cb(null, false); }
-      if (user.password != password) { return cb(null, false); }
-      return cb(null, user);
-    });
-  }));
+setAppPassportMiddleware(app);
 
-app.post('/login',
-  passport.authenticate('local', { successRedirect: '/',
-                                   failureRedirect: '/login'})
-);
-
-app.get('/login', (req,res) => {
-  let data = {
-    formButtonName: "Login",
-    username: null
-  }
-  res.render("pages/login", data)
-})
-
-app.get('/logout', function(req, res){
-  req.logout();
-  res.redirect('/');
-});
-
-app.get('/register', (req, res) => {
-  let data = {
-    formButtonName: "Register",
-    username: null
-  }
-  res.render("pages/register", data);
-})
-
-app.post('/register', (req, res) => {
-  insertUser(req.body.username, req.body.email, req.body.password);
-  res.redirect('/');
-})
 
 
 // render the profile of an user
---
+++ src/api/login.ts
@@ -0,0 +1,78 @@
+import { findUser, insertUser } from "../model/database";
+import express from 'express';
+
+export function setAppPassportMiddleware(app : express.Express){
+    
+    // PASSPORT JS PART
+    var passport = require('passport');
+    var Strategy = require('passport-local').Strategy;
+    var session = require('express-session');
+    var cookieSession = require('cookie-session');
+    var cookieParser = require('cookie-parser');
+
+    passport.serializeUser(function(user :any, done :any) {
+    //console.log("serialization")
+    //console.log(JSON.stringify(user))
+    done(null, JSON.stringify(user));
+    });
+
+    passport.deserializeUser(function(user :any, done :any) {
+    //console.log("Deserialization")
+    //console.log(user)
+    done(null, JSON.parse(user)); // TODO : handle deserialization if parsing fails ( weird cookie input )
+    });
+
+    app.use(cookieParser('secret'));
+    app.use(cookieSession({signed:false}));
+    app.use(session({ secret: 'anything' }));
+
+    app.use(require('body-parser').urlencoded({ extended: true }));
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    app.set('view engine', 'ejs');
+    
+
+    passport.use(new Strategy(
+    function(username : any, password : any, cb : any) {
+        findUser(username, function(err : any, user : any) {
+        if (err) { return cb(err); }
+        if (!user) { return cb(null, false); }
+        if (user.password != password) { return cb(null, false); }
+        return cb(null, user);
+        });
+    }));
+
+    app.post('/login',
+    passport.authenticate('local', { successRedirect: '/',
+                                    failureRedirect: '/login'})
+    );
+
+    app.get('/login', (req, res) => {
+    let data = {
+        formButtonName: "Login",
+        username: null
+    }
+    res.render("pages/login", data)
+    })
+
+    app.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
+    });
+
+    app.get('/register', (req, res) => {
+    let data = {
+        formButtonName: "Register",
+        username: null
+    }
+    res.render("pages/register", data);
+    })
+
+    app.post('/register', (req, res) => {
+    insertUser(req.body.username, req.body.email, req.body.password);
+    res.redirect('/');
+    })
+    
+}
